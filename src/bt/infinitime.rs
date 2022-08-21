6de5ff4fc8096a5a917000c3d5652e989ddb1df4
Andrii Zymohliad <azymohliad@pm.me>
2022-08-21 23:04:47 +0300

    Fix compile error

--- src/bt/infinitime.rs
+++ src/bt/infinitime.rs
@@ -61,7 +61,7 @@ pub struct InfiniTime {
 impl InfiniTime {
     pub async fn new(device: Arc<Device>) -> Result<Self> {
         let mut characteristics = super::CharacteristicsMap::read(&device).await?;
-        dbg!(characteristics);
+        dbg!(&characteristics.0.keys());
         Ok(Self {
             device,
             chr_battery_level: characteristics.take(&uuids::CHR_BATTERY_LEVEL)?,
