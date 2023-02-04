862e3d2791835cddcdea02a59f96ac9b933125f8
Andrii Zymohliad <azymohliad@pm.me>
2023-02-04 11:21:33 +0200

    Minor refactor

--- src/infinitime/bluetooth/mod.rs
+++ src/infinitime/bluetooth/mod.rs
@@ -1,7 +1,3 @@
-use bluer::{Adapter, AdapterEvent, Result, Session};
-use futures::{pin_mut, StreamExt};
-use std::sync::Arc;
-
 mod device;
 mod services;
 mod uuids;
@@ -12,28 +8,3 @@ pub use device::{
     progress_channel,
 };
 pub use services::start_gatt_services;
-
-pub async fn init_adapter() -> Result<Adapter> {
-    let session = Session::new().await?;
-    let adapter = session.default_adapter().await?;
-    adapter.set_powered(true).await?;
-    Ok(adapter)
-}
-
-pub async fn scan(adapter: Arc<Adapter>, callback: impl Fn(AdapterEvent)) {
-    match adapter.discover_devices().await {
-        Ok(events) => {
-            pin_mut!(events);
-
-            loop {
-                tokio::select! {
-                    Some(event) = events.next() => callback(event),
-                    else => break,
-                }
-            }
-        }
-        Err(error) => {
-            log::error!("Scanning failure: {}", error);
-        }
-    }
-}
