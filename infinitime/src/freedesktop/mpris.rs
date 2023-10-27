7fdd2e6121ff32dfbaf4ac2bba552231e71c825c
Andrii Zymohliad <azymohliad@users.noreply.github.com>
2023-10-27 08:26:54 +0300

    Update dependencies (#40)

--- infinitime/src/freedesktop/mpris.rs
+++ infinitime/src/freedesktop/mpris.rs
@@ -54,17 +54,21 @@ pub async fn update_track_metadata(metadata: &Metadata, infinitime: &bt::InfiniT
         .and_then(|s| s.first())
         .map(String::as_str)
         .unwrap_or("Unknown Artist");
+    log::debug!("Artist: {}", artist);
     infinitime.write_mp_artist(artist).await?;
 
     let album = metadata.album();
     let album = album.as_ref().map(String::as_str).unwrap_or("Unknown Album");
+    log::debug!("Album: {}", album);
     infinitime.write_mp_album(album).await?;
 
     let title = metadata.title();
     let track = title.as_ref().map(String::as_str).unwrap_or("Unknown Track");
+    log::debug!("Track: {}", track);
     infinitime.write_mp_track(&track).await?;
 
     let length = metadata.length().unwrap_or_default().as_seconds_f32() as u32;
+    log::debug!("Length: {}", length);
     infinitime.write_mp_duration(length).await?;
     Ok(())
 }
@@ -100,6 +104,7 @@ pub async fn run_control_session(media_player: &MediaPlayer, infinitime: &bt::In
     let player = media_player.player().await?;
 
     // Obtain even streams
+    log::debug!("Creating event streams...");
     let mut playback_status_stream = player.receive_playback_status_changed().await;
     let mut loop_status_stream = player.receive_loop_status_changed().await;
     let mut shuffle_stream = player.receive_shuffle_changed().await;
@@ -114,12 +119,14 @@ pub async fn run_control_session(media_player: &MediaPlayer, infinitime: &bt::In
     pin_mut!(control_event_stream);
 
     // Query player capabilities
+    log::debug!("Querying player capabilities...");
     let mut can_go_next = player.can_go_next().await?;
     let mut can_go_previous = player.can_go_previous().await?;
     let mut can_pause = player.can_pause().await?;
     let mut can_play = player.can_play().await?;
 
     // Send initial player info to the watch
+    log::debug!("Sending player info to the watch...");
     update_player_info(&player, infinitime).await?;
 
     // Process events
@@ -127,6 +134,7 @@ pub async fn run_control_session(media_player: &MediaPlayer, infinitime: &bt::In
     loop {
         tokio::select! {
             Some(event) = control_event_stream.next() => {
+                log::debug!("Control event: {:?}", event);
                 match event {
                     bt::MediaPlayerEvent::AppOpenned => (),
                     bt::MediaPlayerEvent::Play => if can_play {
@@ -153,41 +161,51 @@ pub async fn run_control_session(media_player: &MediaPlayer, infinitime: &bt::In
             }
             Some(property) = playback_status_stream.next() => {
                 let status = PlaybackStatus::from_str(&property.get().await?)?;
+                log::debug!("Playback status: {:?}", status);
                 let is_playing = status == PlaybackStatus::Playing;
                 infinitime.write_mp_playback_status(is_playing).await?;
             }
             Some(property) = loop_status_stream.next() => {
                 let status = LoopStatus::from_str(&property.get().await?)?;
+                log::debug!("Loop status: {:?}", status);
                 let repeat = status == LoopStatus::Track;
                 infinitime.write_mp_repeat(repeat).await?;
             }
             Some(property) = shuffle_stream.next() => {
                 let shuffle = property.get().await?;
+                log::debug!("Shuffle: {:?}", shuffle);
                 infinitime.write_mp_shuffle(shuffle).await?;
             }
             Some(property) = position_stream.next() => {
                 let position = (property.get().await? / 1_000_000) as u32;
+                log::debug!("Position: {:?}", position);
                 infinitime.write_mp_position(position).await?;
             }
             Some(property) = rate_stream.next() => {
                 let rate = property.get().await? as f32;
+                log::debug!("Rate: {:?}", rate);
                 infinitime.write_mp_playback_speed(rate).await?;
             }
             Some(property) = metadata_stream.next() => {
                 let metadata = Metadata::from(property.get().await?);
+                log::debug!("Metadata: {:?}", metadata);
                 update_track_metadata(&metadata, infinitime).await?;
             }
             Some(property) = can_go_next_stream.next() => {
                 can_go_next = property.get().await?;
+                log::debug!("Supports next: {:?}", can_go_next);
             }
             Some(property) = can_go_previous_stream.next() => {
                 can_go_previous = property.get().await?;
+                log::debug!("Supports previous: {:?}", can_go_previous);
             }
             Some(property) = can_pause_stream.next() => {
                 can_pause = property.get().await?;
+                log::debug!("Supports pause: {:?}", can_pause);
             }
             Some(property) = can_play_stream.next() => {
                 can_play = property.get().await?;
+                log::debug!("Supports play: {:?}", can_play);
             }
             else => break,
         }
